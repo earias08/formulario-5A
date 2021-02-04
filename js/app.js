62c5925b6490b42abf961b0c3d19809f237645d6
Emilse Arias <emilsearias@gmail.com>
2021-02-04 11:42:58 -0300

    se agrego la funcion para validar mails, numeros, cantidad de texto y comenzamos con la funcion validar general

--- js/app.js
+++ js/app.js
@@ -1,19 +1,71 @@
-function campoRequerido(elemento){
-    console.log('en la funcion campo requerido')
- //   let elemento = document.querySelector('#nombre');
-    if(elemento.value === ''){
-        elemento.className = 'form-control is-invalid';
+function campoRequerido(elemento) {
+  console.log("en la funcion campo requerido");
+  //   let elemento = document.querySelector('#nombre');
+  if (elemento.value === "") {
+    elemento.className = "form-control is-invalid";
+    return false;
+  } else {
+    elemento.className = "form-control is-valid";
+    return true;
+  }
+}
+
+function validarMail(input) {
+  //emilse@gmail.com
+  let expresion = /\w+@\w+\.[a-z]{2,4}$/;
+  if (input.value != "" && expresion.test(input.value)) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
+}
+
+function validarNumeros(input) {
+  if (input.value != "" && !isNaN(input.value)) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
+
+  /*esto NO es un numero isNaN(---)
+    isNaN('hola') el resultado es true
+    isNaN(12334) el resultado es false
+
+    ! operador Not (negacion)
+    !true -> false
+    !false -> true
+    */
+}
+
+function validarCantidadCaracteres(input){
+    if(input.value != '' && input.value.length >= 10){
+        input.className = 'form-control is-valid';
     }else{
-        elemento.className = 'form-control is-valid';  
+        input.className = "form-control is-invalid";
     }
 }
 
-function validarMail(){
-    console.log('en la funcion campo requerido')
-    let elemento = document.querySelector('#email');
-    if(elemento.value === ''){
-        elemento.className = 'form-control is-invalid';
+//agregar eventos desde js
+let checkbox = document.getElementById('checkTermino');
+// checkbox.addEventListener('change', function (){
+//     //esto es una funcion anonima, funciona solo aqui no la puedo llamar desde otro lugar
+// })
+
+checkbox.addEventListener('change', validarCheck);
+
+function validarCheck(){
+    console.log(checkbox.checked)
+    if(checkbox.checked){ //if(checkbox.checked == true)
+        checkbox.className = 'form-check-input is-valid';
     }else{
-        elemento.className = 'form-control is-valid';  
+        checkbox.className = 'form-check-input is-invalid';
     }
 }
+
+function validarGeneral(event){
+    event.preventDefault();
+    console.log('dentro de la funcion validar general' + event);
+
+    // if(campoRequerido(document.getElementById('nombre')))
+}
+
