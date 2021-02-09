56a2f4124f03c7ec2c3982d6bec2615272c9f948
Emilse Arias <emilsearias@gmail.com>
2021-02-09 11:40:25 -0300

    se agrego la logica para enviar mails usando el servicio de emailjs

--- js/app.js
+++ js/app.js
@@ -15,16 +15,20 @@ function validarMail(input) {
   let expresion = /\w+@\w+\.[a-z]{2,4}$/;
   if (input.value != "" && expresion.test(input.value)) {
     input.className = "form-control is-valid";
+    return true;
   } else {
     input.className = "form-control is-invalid";
+    return false;
   }
 }
 
 function validarNumeros(input) {
   if (input.value != "" && !isNaN(input.value)) {
     input.className = "form-control is-valid";
+    return true;
   } else {
     input.className = "form-control is-invalid";
+    return false;
   }
 
   /*esto NO es un numero isNaN(---)
@@ -40,8 +44,10 @@ function validarNumeros(input) {
 function validarCantidadCaracteres(input){
     if(input.value != '' && input.value.length >= 10){
         input.className = 'form-control is-valid';
+        return true;
     }else{
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -57,15 +63,49 @@ function validarCheck(){
     console.log(checkbox.checked)
     if(checkbox.checked){ //if(checkbox.checked == true)
         checkbox.className = 'form-check-input is-valid';
+        return true;
     }else{
         checkbox.className = 'form-check-input is-invalid';
+        return false;
     }
 }
 
 function validarGeneral(event){
     event.preventDefault();
     console.log('dentro de la funcion validar general' + event);
+    let alerta = document.getElementById('msjEnvio');
 
-    // if(campoRequerido(document.getElementById('nombre')))
+    if(campoRequerido(document.getElementById('nombre')) &&
+    validarMail(document.getElementById('email')) &&
+    validarNumeros(document.getElementById('telefono')) &&
+    validarCantidadCaracteres(document.getElementById('consulta')) &&
+    validarCheck()){
+      enviarMail();
+      // alerta.className = 'alert alert-success mx-3';
+      // alerta.innerHTML = 'Su consulta fue enviada correctamente'; 
+    }else{
+      alerta.className = 'alert alert-danger mx-3';
+      alerta.innerHTML = 'Ocurrio un error, verifique los datos ingresados.'; 
+    }
+}
+
+function enviarMail(){
+  // envioelmail.then(funcion si todo salio bien, funcion cuando algo salio)
+  emailjs.send("service_skyw81f","template_NQ91Q5cI",{
+    to_name: "Administrador",
+    from_name: document.getElementById('email').value,
+    message_html: `Nombre y Apellido: ${document.getElementById('nombre').value} -
+    Telefono: ${document.getElementById('telefono').value} -
+    Consulta: ${document.getElementById('consulta').value}`,
+    }).then(
+      function (response){
+        console.log(response);
+        // agregar el alerta que todo salio bien
+        alert('los datos se enviaron');
+      }, function (error){
+        console.log(error);
+        alert('fallo el envio');
+      }
+    )
 }
 
